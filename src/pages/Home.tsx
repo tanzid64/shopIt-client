192e8527f426a40d60b4f134bd28dacc700d714d
tanzid64 <tanzid@inbox.ru>
2024-07-17 23:26:46 +0600

    home page banner ui completed..

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      {/* Banner */}
+      <section></section>
+      {/* Latest Products */}
+      <h1>
+        Latest Products
+        <Link to="/search" className="find-more">
+          More
+        </Link>
+      </h1>
+      <main></main>
+    </div>
+  );
 };
 export default Home;
